26d5b9f8ec4393cb1fe8351864095b150df740e6
marc-develop <marc310@gmx.de>
2022-09-19 22:16:01 +0200

    g

--- netlify/functions/handle-form/handle-form.js
+++ netlify/functions/handle-form/handle-form.js
@@ -6,13 +6,10 @@ export async function handler(event, context) {
     return { statusCode: 405, body: "Method Not Allowed" };
   }
 
-  // When the method is POST, the name will no longer be in the event’s
-  // queryStringParameters – it’ll be in the event body encoded as a query string
   const params = parse(event.body);
   const { name } = params
   console.log(params);
   const userName = { name }
-
 
   return {
     statusCode: 200,
